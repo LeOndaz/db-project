e3b0c500bb10ba744e05067adfb708de3b6f4436
Ahmed Ibrahim <leondaz@macbook.local>
2023-05-13 06:14:21 +0300

    remove missing import

--- routers/__init__.py
+++ routers/__init__.py
@@ -3,6 +3,5 @@ from .branch import router as branch_router
 from .customer import router as customer_router
 from .insurance import router as insurance_router
 from .inventory import router as inventory_router
-from .misc import router as misc_router
 from .order import router as order_router
 from .product import router as product_router
